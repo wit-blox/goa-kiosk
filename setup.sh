bed0ba3b714c897da55e481622c607ec84d650c3
AamirFromWitblox <aamir@witblox.com>
2023-06-27 19:15:45 +0530

    removed unwanted files

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ cd C:/goa-kiosk
 echo "Starting Server for" $1
 
 npm install
-mkdir sensor-files && mkdir vernier-files
+mkdir sensor-files && mkdir vernier-files && mkdir reveal-files
 cd client && npm install && npm run build && cd ..
 
 npm run $1
